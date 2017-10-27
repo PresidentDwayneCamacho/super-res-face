2ae0894f1284f2a075ed06146b0896983b354f0b
Trey Amador <tmamador@cpp.edu>
2017-10-27 02:57:53 -0700

    working dlib function

--- dlibface.py
+++ dlibface.py
@@ -1,7 +1,9 @@
+#import face_recognition
 from PIL import Image
-import dlib
 import scipy
+import dlib
 import cv2
+import numpy as np
 
 try:
     import face_recognition_models
@@ -10,45 +12,39 @@ except:
     quit()
 
 
-def encoding():
-    pass
-
-
-
-
-
-ground_image = scipy.misc.imread('img/araya.jpg',mode='RGB')
 face_detector = dlib.get_frontal_face_detector()
-
-detector = face_detector(ground_image,1)
-
 predictor_68_point_model = face_recognition_models.pose_predictor_model_location()
 pose_predictor_68_point = dlib.shape_predictor(predictor_68_point_model)
 pose_predictor = pose_predictor_68_point
-
-raw_face_landmarks = [pose_predictor(ground_image,face_location) for face_location in detector]
-ground_encoding = [np.array(face_encoder.compute_face_descriptor(face_image, raw_landmark_set, num_jitters)) for raw_landmark_set in raw_landmarks][0]
-
-
-
-
-
-
 face_recognition_model = face_recognition_models.face_recognition_model_location()
 face_encoder = dlib.face_recognition_model_v1(face_recognition_model)
 
 
+def init_img(filepath):
+    img = np.array(Image.open(filepath))
+    return img
 
 
-ground_encoding =
+def encode_face(face_image):
+    face_locations = face_detector(face_image,1)
+    raw_landmarks = [pose_predictor(face_image, face_location) for face_location in face_locations]
+    return [np.array(face_encoder.compute_face_descriptor(face_image,raw_landmark_set,1)) for raw_landmark_set in raw_landmarks]
 
 
+def recognize_face(face_encodings,face_to_compare):
+    tolerance = 0.6
+    distance = np.linalg.norm(face_encodings - face_to_compare,axis=1)
+    return list(distance <= tolerance)
 
 
+def test():
+    img01 = init_img('img/araya.jpg')
+    encoding01 = encode_face(img01)[0]
+    img02 = init_img('img/unknown.jpg')
+    encoding02 = encode_face(img02)[0]
+    return recognize_face([encoding01],encoding02)
 
-
-
-
+print(test())
 
 
 
